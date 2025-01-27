9c29b4ded46bcf555f7f1c158727b15a5d638283
galihap76 <g4lihanggoro@gmail.com>
2025-01-27 21:49:40 +0700

    feat: menambahkan fitur lupa passsword dan menambahkan validasi tambahan pada bagian settings

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Http\Controllers\Backend\SettingController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,52 +24,112 @@ use Illuminate\Support\Facades\Auth;
 
 // ======= FRONTEND ======= \\
 
-Route::get('/','Frontend\IndexController@index');
+Route::get('/', 'Frontend\IndexController@index');
 
-    ///// MENU \\\\\
-        //// PROFILE SEKOLAH \\\\
-        Route::get('profile-sekolah',[App\Http\Controllers\Frontend\IndexController::class,'profileSekolah'])->name('profile.sekolah');
+///// MENU \\\\\
+//// PROFILE SEKOLAH \\\\
+Route::get('profile-sekolah', [App\Http\Controllers\Frontend\IndexController::class, 'profileSekolah'])->name('profile.sekolah');
 
-        //// VISI dan MISI
-        Route::get('visi-dan-misi',[App\Http\Controllers\Frontend\IndexController::class,'visimisi'])->name('visimisi.sekolah');
+//// VISI dan MISI
+Route::get('visi-dan-misi', [App\Http\Controllers\Frontend\IndexController::class, 'visimisi'])->name('visimisi.sekolah');
 
-        //// PROGRAM STUDI \\\\
-        Route::get('program/{slug}', [App\Http\Controllers\Frontend\MenuController::class, 'programStudi']);
-        //// PROGRAM STUDI \\\\
-        Route::get('kegiatan/{slug}', [App\Http\Controllers\Frontend\MenuController::class, 'kegiatan']);
+//// PROGRAM STUDI \\\\
+Route::get('program/{slug}', [App\Http\Controllers\Frontend\MenuController::class, 'programStudi']);
+//// PROGRAM STUDI \\\\
+Route::get('kegiatan/{slug}', [App\Http\Controllers\Frontend\MenuController::class, 'kegiatan']);
 
-        /// BERITA \\\
-        Route::get('berita',[App\Http\Controllers\Frontend\IndexController::class,'berita'])->name('berita');
-        Route::get('berita/{slug}',[App\Http\Controllers\Frontend\IndexController::class,'detailBerita'])->name('detail.berita');
+/// BERITA \\\
+Route::get('berita', [App\Http\Controllers\Frontend\IndexController::class, 'berita'])->name('berita');
+Route::get('berita/{slug}', [App\Http\Controllers\Frontend\IndexController::class, 'detailBerita'])->name('detail.berita');
 
-        /// EVENT \\\
-        Route::get('event/{slug}',[App\Http\Controllers\Frontend\IndexController::class,'detailEvent'])->name('detail.event');
-        Route::get('event',[App\Http\Controllers\Frontend\IndexController::class,'events'])->name('event');
+/// EVENT \\\
+Route::get('event/{slug}', [App\Http\Controllers\Frontend\IndexController::class, 'detailEvent'])->name('detail.event');
+Route::get('event', [App\Http\Controllers\Frontend\IndexController::class, 'events'])->name('event');
 
 Auth::routes(['register' => false]);
 
 
 // ======= BACKEND ======= \\
+Route::middleware('guest')->group(function () {
+    Route::get('/forgot-password', function () {
+        return view('auth.forgot-password');
+    })->name('password.request');
+
+    Route::post('/forgot-password', function (Request $request) {
+
+        function statusFunction($status)
+        {
+            return $status;
+        }
+
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? back()->with(['status' => __(statusFunction('Link reset password telah berhasil dikirim melalui email. Silakan periksa email Anda.'))])
+            : back()->withErrors(['email' => __(statusFunction('Email tidak ada.'))]);
+    })->name('password.email');
+
+    Route::get('/reset-password/{token}', function ($token) {
+        return view('auth.reset-password', ['token' => $token]);
+    })->name('password.reset');
+
+    Route::post('/reset-password', function (Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required',
+        ], [
+            'password.required' => 'Password harus di isi.',
+            'password.min' => 'Password setidaknya minimal 8 karakter.',
+            'password.confirmed' => 'Konfirmasi password tidak cocok.',
+            'password_confirmation.required' => 'Konfirmasi password harus di isi.',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
+    })->name('password.update');
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-     /// PROFILE \\\
-    Route::resource('profile-settings',Backend\ProfileController::class);
+    /// PROFILE \\\
+    Route::resource('profile-settings', Backend\ProfileController::class);
     /// SETTINGS \\\
-      Route::prefix('settings')->group( function(){
+    Route::prefix('settings')->group(function () {
         // BANK
-        Route::get('/',[App\Http\Controllers\Backend\SettingController::class,'index'])->name('settings');
+        Route::get('/', [App\Http\Controllers\Backend\SettingController::class, 'index'])->name('settings');
         // TAMBAH BANK
-        Route::post('add-bank',[App\Http\Controllers\Backend\SettingController::class,'addBank'])->name('settings.add.bank');
+        Route::post('add-bank', [App\Http\Controllers\Backend\SettingController::class, 'addBank'])->name('settings.add.bank');
         // NOTIFICATIONS
-        Route::put('notifications/{id}',[SettingController::class,'notifications']);
-      });
+        Route::put('notifications/{id}', [SettingController::class, 'notifications']);
+    });
 
 
     /// CHANGE PASSWORD
-    Route::put('profile-settings/change-password/{id}',[App\Http\Controllers\Backend\ProfileController::class, 'changePassword'])->name('profile.change-password');
+    Route::put('profile-settings/change-password/{id}', [App\Http\Controllers\Backend\ProfileController::class, 'changePassword'])->name('profile.change-password');
 
-    Route::prefix('/')->middleware('role:Admin')->group( function (){
+    Route::prefix('/')->middleware('role:Admin')->group(function () {
         ///// WEBSITE \\\\\
         Route::resources([
             /// PROFILE SEKOLAH \\
